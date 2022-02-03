7a53e3c83ed1c5e1e2bf5306a06409fe200197c4
Richard Simpson <richard.simpson235@gmail.com>
2022-02-03 14:27:37 -0500

    Add exception throwing declarations

--- src/main/java/com/revature/services/CatService.java
+++ src/main/java/com/revature/services/CatService.java
@@ -1,14 +1,16 @@
 package com.revature.services;
 
+import com.revature.exceptions.PersistenceException;
 import com.revature.models.Cat;
 
+import javax.naming.ServiceUnavailableException;
 import java.util.List;
 
 public interface CatService {
 
-    public void deleteCat(Cat cat);
-    public void createCat(Cat cat);
-    public Cat getCatById(int id);
-    public List<Cat> getAllCats();
-    public void updateCat(Cat cat);
+    public void deleteCat(Cat cat) throws PersistenceException, ServiceUnavailableException;
+    public void createCat(Cat cat) throws PersistenceException, ServiceUnavailableException;
+    public Cat getCatById(int id) throws PersistenceException, ServiceUnavailableException;
+    public List<Cat> getAllCats() throws PersistenceException, ServiceUnavailableException;
+    public void updateCat(Cat cat) throws PersistenceException, ServiceUnavailableException;
 }
