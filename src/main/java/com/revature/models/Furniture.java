920c0a63b4cda8aaf6a610260a8b3be9cbc8cabc
Bobby <bodavis@csumb.edu>
2022-02-03 12:23:20 -0800

    Added furniture model

--- src/main/java/com/revature/models/Furniture.java
+++ src/main/java/com/revature/models/Furniture.java
@@ -1,4 +1,33 @@
 package com.revature.models;
 
+//@Entity
 public class Furniture {
+//    @Id
+    int furnitureId;
+    String type;
+    boolean cozy;
+
+    public Furniture(){}
+    public Furniture(String type, boolean cozy){
+        this.type = type;
+        this.cozy = cozy;
+    }
+
+    public int getFurnitureId() {
+        return furnitureId;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isCozy() {
+        return cozy;
+    }
+    public void setCozy(boolean cozy) {
+        this.cozy = cozy;
+    }
 }
