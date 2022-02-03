81cf57a31de1fcada96958b9bfe933e0d2c02f82
Richard Simpson <richard.simpson235@gmail.com>
2022-02-03 16:22:36 -0500

    Register FurnitureServlet

--- src/main/java/com/revature/Bootstrapper.java
+++ src/main/java/com/revature/Bootstrapper.java
@@ -3,7 +3,11 @@ package com.revature;
 import com.revature.annotations.URI;
 import com.revature.controllers.CatController;
 import com.revature.controllers.CatControllerImpl;
+import com.revature.controllers.FurnitureController;
+import com.revature.controllers.FurnitureControllerImpl;
+import com.revature.models.Furniture;
 import com.revature.servlets.CatServlet;
+import com.revature.servlets.FurnitureServlet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,12 +28,24 @@ public class Bootstrapper implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
 
         ServletContext context = sce.getServletContext();
+        initializeCatServlet(context);
+        initializeFurnitureServlet(context);
 
+        logger.info(context.getServletRegistration("CatServlet").getMappings());
+        logger.info(context.getServletRegistration("FurnitureServlet").getMappings());
+    }
+
+    private void initializeCatServlet(ServletContext context) {
         CatController catController = new CatControllerImpl(null);
         ServletRegistration.Dynamic registration = context.addServlet("CatServlet", new CatServlet(catController));
         URI paths = CatServlet.class.getAnnotation(URI.class);
         Arrays.stream(paths.urlPatterns()).forEach(registration::addMapping);
+    }
 
-        logger.info(context.getServletRegistration("CatServlet").getMappings());
+    private void initializeFurnitureServlet(ServletContext context) {
+        FurnitureController furnitureController = new FurnitureControllerImpl(null);
+        ServletRegistration.Dynamic registration = context.addServlet("FurnitureServlet", new FurnitureServlet(furnitureController));
+        URI paths = FurnitureServlet.class.getAnnotation(URI.class);
+        Arrays.stream(paths.urlPatterns()).forEach(registration::addMapping);
     }
 }
