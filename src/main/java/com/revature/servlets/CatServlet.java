ccb692bcb07f1a2c79119130f9e6d5c9751a1afa
Richard Simpson <richard.simpson235@gmail.com>
2022-02-03 14:01:37 -0500

    Implement doPost for handling post requests

--- src/main/java/com/revature/servlets/CatServlet.java
+++ src/main/java/com/revature/servlets/CatServlet.java
@@ -30,9 +30,9 @@ public class CatServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String[] uri = request.getRequestURI().split("/");
+        String[] uriTokens = request.getRequestURI().split("/");
 
-        switch(uri.length) {
+        switch(uriTokens.length) {
             case 0:
             case 1:
             case 2: {
@@ -40,10 +40,10 @@ public class CatServlet extends HttpServlet {
                 break;
             }
             case 3: {
-                if(!(request.getQueryString() == null) && "cats".equals(uri[2])) {
+                if(!(request.getQueryString() == null) && "cats".equals(uriTokens[2])) {
                     // we have query parameter
                     response.sendError(501);
-                } else if(request.getQueryString() == null && "cats".equals(uri[2])) {
+                } else if(request.getQueryString() == null && "cats".equals(uriTokens[2])) {
                     // we have a request for all cats
                     this.controller.getAllCats(request, response);
                 } else {
@@ -52,12 +52,49 @@ public class CatServlet extends HttpServlet {
                 break;
             }
             case 4: {
-                if(uri[3].matches("[0-9]+")) {
+                if(uriTokens[3].matches("[0-9]+")) {
                     // we have a request for a specific cat
-                    request.setAttribute("id", uri[3]);
+                    request.setAttribute("id", uriTokens[3]);
                     this.controller.getCatById(request, response);
                 } else {
                     response.sendError(400, "Only id values are valid.");
+                }
+                break;
+            }
+            default: {
+                response.sendError(400);
+                break;
+            }
+        }
+    }
+
+    /**
+     * This method handles POST requests made to the cats uri. It parses the url to determine which
+     * controller method needs to be called.
+     *
+     * @param request                  the request to handle
+     * @param response                 the response to return to the client
+     * @throws IOException             thrown when sending an error response fails
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String[] uriTokens = request.getRequestURI().split("/");
+
+        switch(uriTokens.length) {
+            case 0:
+            case 1:
+            case 2: {
+                response.sendError(404);
+                break;
+            }
+            case 3: {
+                if(!(request.getQueryString() == null) && "cats".equals(uriTokens[2])) {
+                    response.sendError(501);
+                } else if(request.getQueryString() == null && "cats".equals(uriTokens[2])) {
+                    // we post request for a cat
+                    this.controller.createCat(request, response);
+                } else {
+                    response.sendError(400);
                 }
                 break;
             }
