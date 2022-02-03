ccd4f5b2ed274babbf1c31d889dd7d795f5d6f7e
Richard Simpson <richard.simpson235@gmail.com>
2022-02-03 14:28:25 -0500

    Add exception throwing declarations

--- src/main/java/com/revature/services/FurnitureService.java
+++ src/main/java/com/revature/services/FurnitureService.java
@@ -1,14 +1,16 @@
 package com.revature.services;
 
+import com.revature.exceptions.PersistenceException;
 import com.revature.models.Furniture;
 
+import javax.naming.ServiceUnavailableException;
 import java.util.List;
 
 public interface FurnitureService {
 
-    public void deleteFurniture(Furniture furniture);
-    public void createFurniture(Furniture furniture);
-    public Furniture getFurnitureById(int id);
-    public List<Furniture> getAllFurniture();
-    public void updateFurniture(Furniture furniture);
+    public void deleteFurniture(Furniture furniture) throws PersistenceException, ServiceUnavailableException;
+    public void createFurniture(Furniture furniture) throws PersistenceException, ServiceUnavailableException;
+    public Furniture getFurnitureById(int id) throws PersistenceException, ServiceUnavailableException;
+    public List<Furniture> getAllFurniture() throws PersistenceException, ServiceUnavailableException;
+    public void updateFurniture(Furniture furniture) throws PersistenceException, ServiceUnavailableException;
 }
