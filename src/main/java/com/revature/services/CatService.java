a1cf7765321c67e7dc032b2b9f886f1418eada67
Richard Simpson <richard.simpson235@gmail.com>
2022-02-04 15:22:33 -0500

    Change getById to return Optional

--- src/main/java/com/revature/services/CatService.java
+++ src/main/java/com/revature/services/CatService.java
@@ -5,13 +5,14 @@ import com.revature.models.Cat;
 
 import javax.naming.ServiceUnavailableException;
 import java.util.List;
+import java.util.Optional;
 
 public interface CatService {
 
     public void deleteCat(Cat cat, String sessionId) throws PersistenceException, ServiceUnavailableException;
     public void createCat(Cat cat, String sessionId) throws PersistenceException, ServiceUnavailableException;
     public void createMany(List<Cat> cat, String sessionId) throws PersistenceException, ServiceUnavailableException;
-    public Cat getCatById(int id, String sessionId) throws PersistenceException, ServiceUnavailableException;
+    public Optional<Cat> getCatById(int id, String sessionId) throws PersistenceException, ServiceUnavailableException;
     public List<Cat> getAllCats(String sessionId) throws PersistenceException, ServiceUnavailableException;
     public void updateCat(Cat cat, String sessionId) throws PersistenceException, ServiceUnavailableException;
 }
