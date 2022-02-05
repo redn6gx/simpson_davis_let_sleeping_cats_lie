bdf4359b88e72b3635bdaa4da6ccbaa5713038aa
Bobby <bodavis@csumb.edu>
2022-02-05 07:20:23 -0800

    Added CatControllerTest file with skeleton.

---
+++ src/test/java/controllers/CatControllerTest.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class CatControllerTest {
+
+    @Test
+    public void testDeleteCatSuccess(){}
+    @Test
+    public void testDeleteCatFailed(){}
+
+    @Test
+    public void testCreateCatSuccess(){}
+    @Test
+    public void testCreateCatFailed(){}
+
+    @Test
+    public void testGetCatByIdSuccess(){}
+    @Test
+    public void testGetCatByIdFailed(){}
+
+    @Test
+    public void testGetAllCatsSuccess(){}
+    @Test
+    public void testGetAllCatsFailed(){}
+
+    @Test
+    public void testUpdateCatSuccess(){}
+    @Test
+    public void testUpdateCatFailed(){}
+}
