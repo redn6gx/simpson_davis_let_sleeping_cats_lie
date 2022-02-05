964e2dec91f5a9306c5f4b7e4b256fb05a825bc8
Bobby <bodavis@csumb.edu>
2022-02-04 23:41:48 -0800

    Implemented CatControllerImpl.

--- src/main/java/com/revature/controllers/CatControllerImpl.java
+++ src/main/java/com/revature/controllers/CatControllerImpl.java
@@ -12,6 +12,7 @@ import javax.naming.ServiceUnavailableException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -66,27 +67,165 @@ public class CatControllerImpl implements CatController{
             }
             logger.error(e.getMessage());
         }
-
-
     }
 
     @Override
     public void createCat(HttpServletRequest request, HttpServletResponse response) {
 
+        boolean isMany = false;
+        try {
+            char c = request.getReader().toString().charAt(0);
+            if(c == '['){
+                isMany = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(!isMany) {
+            Cat cat = null;
+            try {
+                cat = gson.fromJson(request.getReader(), Cat.class);
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+            }
+
+            //if multiple json object call cs.createMany, else call cs.createCat
+            try {
+                cs.createCat(cat, request.getSession().getId());
+            } catch (PersistenceException e) {
+                try {
+                    response.sendError(400);
+                } catch (IOException ex) {
+                    logger.error(ex.getMessage());
+                }
+                logger.error(e.getMessage());
+            } catch (ServiceUnavailableException e) {
+                try {
+                    response.sendError(503);
+                } catch (IOException ex) {
+                    logger.error(ex.getMessage());
+                }
+                logger.error(e.getMessage());
+            }
+        }else {
+            //createMany
+            List<Cat> cats = new ArrayList<>();
+
+            try {
+                cs.createMany(cats, request.getSession().getId());
+            } catch (PersistenceException e) {
+                try {
+                    response.sendError(400);
+                } catch (IOException ex) {
+                    logger.error(ex.getMessage());
+                }
+                logger.error(e.getMessage());
+            } catch (ServiceUnavailableException e) {
+                try {
+                    response.sendError(503);
+                } catch (IOException ex) {
+                    logger.error(ex.getMessage());
+                }
+                logger.error(e.getMessage());
+            }
+        }
     }
 
     @Override
     public Cat getCatById(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        String input = request.getAttribute("id").toString();
+        int id=0;
+        if(input.matches("[0-9]+")) {
+            id = Integer.parseInt(input);
+        } else {
+            try{
+                response.sendError(400, "ID is not a number");
+            }catch(IOException e){
+                logger.error(e.getMessage());
+            }
+            return null;
+        }
+
+        Cat cat = null;
+        try{
+            cat = gson.fromJson(request.getReader(), Cat.class);
+        }catch(IOException e){
+            logger.error(e.getMessage());
+        }
+
+        try {
+            cs.getCatById(id, request.getSession().getId());
+        } catch (PersistenceException e) {
+            try {
+                response.sendError(400);
+            } catch (IOException ex) {
+                logger.error(ex.getMessage());
+            }
+            logger.error(e.getMessage());
+        } catch (ServiceUnavailableException e) {
+            try {
+                response.sendError(503);
+            } catch (IOException ex) {
+                logger.error(ex.getMessage());
+            }
+            logger.error(e.getMessage());
+        }
+
+        return cat;
     }
 
     @Override
     public List<Cat> getAllCats(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        List<Cat> cats = new ArrayList<>();
+
+        try {
+            cats = cs.getAllCats(request.getSession().getId());
+        } catch (PersistenceException e) {
+            try {
+                response.sendError(400);
+            } catch (IOException ex) {
+                logger.error(ex.getMessage());
+            }
+            logger.error(e.getMessage());
+        } catch (ServiceUnavailableException e) {
+            try {
+                response.sendError(503);
+            } catch (IOException ex) {
+                logger.error(ex.getMessage());
+            }
+            logger.error(e.getMessage());
+        }
+
+        return cats;
     }
 
     @Override
     public void updateCat(HttpServletRequest request, HttpServletResponse response) {
+        Cat cat = null;
 
+        try {
+            cat = gson.fromJson(request.getReader(), Cat.class);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+
+        try {
+            cs.updateCat(cat, request.getSession().getId());
+        } catch (PersistenceException e) {
+            try {
+                response.sendError(400);
+            } catch (IOException ex) {
+                logger.error(ex.getMessage());
+            }
+            logger.error(e.getMessage());
+        } catch (ServiceUnavailableException e) {
+            try {
+                response.sendError(503);
+            } catch (IOException ex) {
+                logger.error(ex.getMessage());
+            }
+            logger.error(e.getMessage());
+        }
     }
 }
