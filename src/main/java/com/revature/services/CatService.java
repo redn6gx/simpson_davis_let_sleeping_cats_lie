4209461c8eb3dd9d1823a088e85791f41e8e6bd8
Richard Simpson <richard.simpson235@gmail.com>
2022-02-06 22:05:51 -0500

    Document CatService

--- src/main/java/com/revature/services/CatService.java
+++ src/main/java/com/revature/services/CatService.java
@@ -9,10 +9,61 @@ import java.util.Optional;
 
 public interface CatService {
 
+    /**
+     * This method deletes a Cat entity from the database.
+     *
+     * @param cat                              the Cat to be deleted
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     public void deleteCat(Cat cat, String sessionId) throws PersistenceException, ServiceUnavailableException;
+
+    /**
+     * This method adas a Cat entity to the database.
+     *
+     * @param cat                              the Cat to be added
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     public void createCat(Cat cat, String sessionId) throws PersistenceException, ServiceUnavailableException;
+
+    /**
+     * This method adds multiple Cat entities to the database.
+     * @param cats                             the Cat entities to be added
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     public void createManyCats(List<Cat> cats, String sessionId) throws PersistenceException, ServiceUnavailableException;
+
+    /**
+     * This method gets a Cat entity by its id.
+     *
+     * @param id                               the ud of the Cat to be found
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     public Optional<Cat> getCatById(int id, String sessionId) throws PersistenceException, ServiceUnavailableException;
+
+    /**
+     * This method gets all Cat entities from the database.
+     *
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     public List<Cat> getAllCats(String sessionId) throws PersistenceException, ServiceUnavailableException;
+
+    /**
+     * This method updates a Cat entity to the database.
+     *
+     * @param cat                              the Cat to be updated
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     public void updateCat(Cat cat, String sessionId) throws PersistenceException, ServiceUnavailableException;
 }
--- src/main/java/com/revature/services/CatServiceImpl.java
+++ src/main/java/com/revature/services/CatServiceImpl.java
@@ -20,6 +20,14 @@ public class CatServiceImpl implements CatService {
         this.factory = factory;
     }
 
+    /**
+     * This method deletes a Cat entity from the database.
+     *
+     * @param cat                              the Cat to be deleted
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     @Override
     public void deleteCat(Cat cat, String sessionId) throws PersistenceException, ServiceUnavailableException {
         try {
@@ -34,6 +42,14 @@ public class CatServiceImpl implements CatService {
         }
     }
 
+    /**
+     * This method adas a Cat entity to the database.
+     *
+     * @param cat                              the Cat to be added
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     @Override
     public void createCat(Cat cat, String sessionId) throws PersistenceException, ServiceUnavailableException {
         try {
@@ -48,6 +64,13 @@ public class CatServiceImpl implements CatService {
         }
     }
 
+    /**
+     * This method adds multiple Cat entities to the database.
+     * @param cats                             the Cat entities to be added
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     @Override
     public void createManyCats(List<Cat> cats, String sessionId) throws PersistenceException, ServiceUnavailableException {
         EntityManager session;
@@ -80,6 +103,14 @@ public class CatServiceImpl implements CatService {
         }
     }
 
+    /**
+     * This method gets a Cat entity by its id.
+     *
+     * @param id                               the ud of the Cat to be found
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     @Override
     public Optional<Cat> getCatById(int id, String sessionId) throws PersistenceException, ServiceUnavailableException {
         try {
@@ -95,6 +126,13 @@ public class CatServiceImpl implements CatService {
         }
     }
 
+    /**
+     * This method gets all Cat entities from the database.
+     *
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     @Override
     public List<Cat> getAllCats(String sessionId) throws PersistenceException, ServiceUnavailableException {
         try {
@@ -110,6 +148,14 @@ public class CatServiceImpl implements CatService {
         }
     }
 
+    /**
+     * This method updates a Cat entity to the database.
+     *
+     * @param cat                              the Cat to be updated
+     * @param sessionId                        the user's session id
+     * @throws PersistenceException            thrown when a problem occurs with modifying the database
+     * @throws ServiceUnavailableException     thrown when connecting to the database is unavailable
+     */
     @Override
     public void updateCat(Cat cat, String sessionId) throws PersistenceException, ServiceUnavailableException {
         try {
